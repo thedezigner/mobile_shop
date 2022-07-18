27fda62f2fde15470a6cfb4a6032a5817ef36763
thedezigner <thedezigner4u@gmail.com>
2022-07-19 01:04:18 +0530

    Initial commit 4

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
+// $(document).ready(function(){
+
+//     $('.offerItemTitle').click(function(){
+
+//       $(this).parents('.offerslide').children('.offerItem').removeClass('active');
+//       $(this).parents('.offerslide').children('.offerItem').children('.offerItemTitle').removeClass('hide');
+//       $(this).parent('.offerItem').addClass('active');
+//       $(this).addClass('hide');
+
+//     });
+// });
+
+
 $(document).ready(function(){
 
-    $('.offerItemTitle').click(function(){
+  $('.offerItem').click(function(){
 
-      $(this).parents('.offerslide').children('.offerItem').removeClass('active');
-      $(this).parents('.offerslide').children('.offerItem').children('.offerItemTitle').removeClass('hide');
-      $(this).parent('.offerItem').addClass('active');
-      $(this).addClass('hide');
-
-    });
+    $('.offerItem').removeClass('active');
+    $('.offerItemTitle').removeClass('hide');
+    $(this).addClass('active');
+    $(this).children('.offerItemTitle').addClass('hide');
+  });
 });
