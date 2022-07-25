f0a453e221510aad683d30e4c2fb6721f1f1308c
thedezigner <thedezigner4u@gmail.com>
2022-07-26 01:21:10 +0530

    Responsive Remaining

--- js/script.js
+++ js/script.js
@@ -21,3 +21,28 @@ $(document).ready(function(){
     $(this).children('.offerItemTitle').addClass('hide');
   });
 });
+
+
+$(document).ready(function(){
+  $('.instagram_post').slick({
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+          {
+          breakpoint: 769,
+          settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+          }
+          },
+          {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 2
+          }
+          }
+      ]
+  });
+});
